e956e7b7acf5d756a6672fff903960612fc4b1b2
DHINESH S <37235650+dhinesh22@users.noreply.github.com>
2019-08-13 14:00:42 +0530

    Create PrintRepeatedNo.py

---
+++ PrintRepeatedNo.py
@@ -0,0 +1,18 @@
+a = int(input())
+b = list(map(int, input().split()))
+dup = []
+for i in range(0, a):
+    first = b[i]
+    j = 0
+    j = i+1
+    while j < a:
+        second = b[j]
+        if first != second:
+            j += 1
+        elif first == second:
+            dup.append(first)
+            j = a+1
+dup.sort()
+dup = {x for x in dup}
+for y in dup:
+    print(y, end=" ")
